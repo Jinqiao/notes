1eab52d3fc3baf77a36339311b75b75ed1c4d40b
Jinqiao <lijinqiao222@gmail.com>
2018-09-04 20:55:32 +0000

    add cs how to use native lib

---
+++ cs/UseNativeLib/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UseNativeLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2) {
+                Console.WriteLine("need at least 2 arguments");
+            }
+
+            double a = double.Parse(args[0]);
+            double b = double.Parse(args[1]);
+            Console.WriteLine($"my_add: {MyAdd(a, b)}");
+            Console.WriteLine($"my_minus: {MyMinus(a, b)}");
+        }
+
+        // this will looks for libmylib.so on Linux
+        [DllImport("mylib", EntryPoint = "my_add")]
+        private static extern double MyAdd(double a, double b);
+
+        [DllImport("mylib", EntryPoint = "my_minus")]
+        private static extern double MyMinus(double a, double b);
+    }
+}

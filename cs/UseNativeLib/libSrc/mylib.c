ae129e1c6fe9bdbcdefbe34926ca734076a7701f
jinqiao <lijinqiao222@gmail.com>
2019-11-13 14:31:18 -0500

    minor change of mylib.c

--- cs/UseNativeLib/libSrc/mylib.c
+++ cs/UseNativeLib/libSrc/mylib.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-
 // to compile, see https://stackoverflow.com/a/14884166
 // $ gcc -shared -o libmylib.so -fPIC mylib.c
+// See HOWTO file to compile on Windows
 
 double my_add(double a, double b)
 {
